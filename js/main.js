1915867e4459b6c92f28606ce734a46a013f0a18
RoberthJose <152071780+RoberthJose@users.noreply.github.com>
2025-12-10 16:14:25 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-<script>
-  // Detectar clic en el botón hamburguesa
-  const menuToggle = document.querySelector('.menu-toggle');
-  const sidebarLeft = document.querySelector('.sidebar-left');
-  const sidebarRight = document.querySelector('.sidebar-right');
+// Seleccionamos elementos
+const menuToggle = document.querySelector('.menu-toggle');
+const sidebarLeft = document.querySelector('.sidebar-left');
 
-  menuToggle.addEventListener('click', () => {
-    // Alternar clase .active para mostrar/ocultar
-    sidebarLeft.classList.toggle('active');
-    sidebarRight.classList.toggle('active');
-  });
-</script>
+// Evento click en hamburguesa
+menuToggle.addEventListener('click', () => {
+  sidebarLeft.classList.toggle('active');
+});
