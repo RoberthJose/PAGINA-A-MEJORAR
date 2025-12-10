6402c14f723edd385b309b546f5a94cbeca9f4e4
RoberthJose <152071780+RoberthJose@users.noreply.github.com>
2025-12-10 16:21:52 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 // Seleccionamos elementos
 const menuToggle = document.querySelector('.menu-toggle');
 const sidebarLeft = document.querySelector('.sidebar-left');
+const userOptions = document.querySelector('.user-options');
 
-// Evento click en hamburguesa
+// Función para abrir/cerrar menú en móvil
 menuToggle.addEventListener('click', () => {
-  sidebarLeft.classList.toggle('active');
+  // Alterna clase para mostrar/ocultar sidebar
+  sidebarLeft.classList.toggle('show-menu');
+  userOptions.classList.toggle('show-menu');
 });
